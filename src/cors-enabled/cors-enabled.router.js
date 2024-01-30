3f16a25bf8c4cc84dd7dd1edc8e67e2ee56c61e1
sedo-swe <sedo7oh@gmail.com>
2024-01-29 19:02:16 -0500

    Push changes from the lecture

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,17 +1,27 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+// const corsDelete = cors({methods: "DELETE"});
+
+// Add cors into router
+// router.use(cors());
 
 router
   .route("/:corsId")
+  // .all(cors()) // Add cors into single route
   .get(controller.read)
   .put(controller.update)
   .delete(controller.delete)
+  // .delete(cors(), controller.delete)
+  // .options(corsDelete)
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .get(controller.list)
+  // .get(controller.list)  // Add cors into single method of single route
+  .get(cors(), controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
